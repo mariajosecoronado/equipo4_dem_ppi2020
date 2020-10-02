f0e8ff9e586fe19342d60cda04066c227b23ba69
Estefania Builes Quintero <66442870+estefania2013builes@users.noreply.github.com>
2020-10-02 07:20:34 -0500

    se guardo una nueva imagen

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Inicio from "../pages/inicio";
+import Apertura from "../pages/apertura";
 import Registro from "../pages/registro";
 import Menu from "../pages/menu";
+import Bienvenida from "../pages/bienvenida";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Inicio} />
+        <Route exact path="/" component={Apertura} />
         <Route exact path="/registro" component={Registro} />
         <Route exact path="/menu" component={Menu} />
+        <Route exact path="/menu" component={Bienvenida} />
       </Switch>
     </BrowserRouter>
   );
