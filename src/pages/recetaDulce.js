08a1b991c8423031d26116b532905fe226fb255f
Valentina Cossio Parra <66125408+ValentinaC2004@users.noreply.github.com>
2020-10-12 00:25:27 -0300

    pantalla de recetas dulces

--- src/pages/recetaDulce.js
+++ src/pages/recetaDulce.js
@@ -1,9 +1,50 @@
 import React from "react";
 import "../style/style.css";
+import { Nav } from "react-bootstrap";
+import { BsList } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import ReactPlayer from "react-player";
 
 class recetaDulce extends React.Component {
   render() {
-    return <div>PANTALLA DE RECETAS DULCES</div>;
+    return (
+      <div>
+        <Nav className="justify-content-center " activeKey="/home">
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" className="encabezado">
+              <h1>
+                <BsList />
+              </h1>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <h1 className="titulo">COOK TO WORK</h1>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Busqueda" className="encabezado">
+              <h1>
+                {" "}
+                <BsSearch />{" "}
+              </h1>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item></Nav.Item>
+        </Nav>
+        <br></br>
+        <div>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=JuewYWvosn4"
+            className="react-player"
+            playing
+            width="100%"
+            height="270px"
+          />
+          <p className="text-muted  derec">Publicado el 11/09/2020</p>
+        </div>
+      </div>
+    );
   }
 }
 
