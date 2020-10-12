4197114eb66e33ef38057b3653c28f305c993392
Valentina Cossio Parra <66125408+ValentinaC2004@users.noreply.github.com>
2020-10-12 05:46:26 -0300

    Se organizo la navegabilidad

--- src/pages/mensaje.js
+++ src/pages/mensaje.js
@@ -5,7 +5,7 @@ import "../style/style.css";
 class mensaje extends React.Component {
   render() {
     return <div>PANTALLA DE MENSAJES_USUARIO
-<Nav.Link href="menu" >
+<Nav.Link href="menu">
   IR AL MENU
 </Nav.Link>
     </div>;
