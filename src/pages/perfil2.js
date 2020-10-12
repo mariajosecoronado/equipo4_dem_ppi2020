dd6f6d7aece92b8a745c316cbbe1ba3e16403c9e
Mariajosé Coronado Del Castillo <66452068+mariajosecoronado@users.noreply.github.com>
2020-10-12 12:15:04 -0500

    Otro usuario

--- src/pages/perfil2.js
+++ src/pages/perfil2.js
@@ -1,9 +1,46 @@
 import React from "react";
 import "../style/style.css";
+import { BsList } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import { Nav } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 class perfil2 extends React.Component {
   render() {
-    return <div>soy el perfil de otro usuario</div>;
+    return <div>
+        <Nav className="justify-content-center " activeKey="/home">
+          <Nav.Item>
+            <Nav.Link eventKey="link-1" className="encabezado" href="menu">
+              <h1>
+                <BsList />
+              </h1>
+            </Nav.Link>
+          </Nav.Item>
+
+          <img
+            className="icon-p"
+            src="https://raw.githubusercontent.com/Saracas-022/holamundo/main/logotipo.jpeg"
+            alt="logo"
+            width="200"
+            height="150"
+          />
+
+          <Nav.Item>
+            <Nav.Link eventKey="Busqueda" className="encabezado ">
+              <h1>
+                {" "}
+                <BsSearch />{" "}
+              </h1>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item></Nav.Item>
+        </Nav>
+
+    </div>;
   }
 }
 export default perfil2;
