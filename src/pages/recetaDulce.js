0de7607085646eccaad07641e1711d7292cc01f3
Valentina Cossio Parra <66125408+ValentinaC2004@users.noreply.github.com>
2020-10-12 02:59:16 -0300

    botones-public

--- src/pages/recetaDulce.js
+++ src/pages/recetaDulce.js
@@ -76,9 +76,8 @@ class recetaDulce extends React.Component {
         </div>
         <div className="public-flex">
           <Card border="success" style={{ width: "18rem" }}>
-            <Card.Header>Header</Card.Header>
+            <Card.Header>Ingredientes</Card.Header>
             <Card.Body>
-              <Card.Title>Success Card Title</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -89,13 +88,16 @@ class recetaDulce extends React.Component {
           <Card border="success" style={{ width: "18rem" }}>
             <Card.Header>Header</Card.Header>
             <Card.Body>
-              <Card.Title>Success Card Title</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
             </Card.Body>
           </Card>
+        </div>
+        <div>
+          <button>Descargar</button>
+          <button>Compartir</button>
         </div>
       </div>
     );
