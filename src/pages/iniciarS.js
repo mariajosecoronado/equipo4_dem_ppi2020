65e37825a2e10088fa48ca9a14b2857370b22dbb
Estefania Builes Quintero <66442870+estefania2013builes@users.noreply.github.com>
2020-10-19 23:14:44 -0500

    inicio aninimo listo

--- src/pages/iniciarS.js
+++ src/pages/iniciarS.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../style/style.css";
 import { Form, Button } from "react-bootstrap";
 
-
 class iniciarS extends React.Component {
   render() {
     return (
@@ -14,18 +13,18 @@ class iniciarS extends React.Component {
               src="https://raw.githubusercontent.com/Saracas-022/holamundo/main/logotipo.jpeg"
               alt="logo"
               width="100"
-              
-              
             />
 
             <h1 className="titulo-IR">
-           
               BIENVENID@ A COOK TO WORK <br />
               INICIE SESIÓN AQUÍ
             </h1>
             <div>
-              <Form >
-                <Form.Group controlId="formBasicEmail" className="campo-iniciarS">
+              <Form>
+                <Form.Group
+                  controlId="formBasicEmail"
+                  className="campo-iniciarS"
+                >
                   <Form.Control type="email" placeholder="Correo:" />
                 </Form.Group>
 
@@ -35,20 +34,22 @@ class iniciarS extends React.Component {
                 >
                   <Form.Control type="password" placeholder="Contraseña:" />
                 </Form.Group>
-                <p><a href="/olvidaste_contraseña" className="O-contraseña">¿Has olvidado tu contraseña?</a></p>
-                
-                
+                <p>
+                  <a href="/olvidaste_contraseña" className="O-contraseña">
+                    ¿Has olvidado tu contraseña?
+                  </a>
+                </p>
 
                 <Button href="/inicio" className="boton-I">
-                   INICIAR SESIÓN COMO USUARIO
+                  INICIAR SESIÓN COMO USUARIO
                 </Button>
                 <br />
-                <Button  href="/inicio_admin" className="boton-I">
-                 INICIAR SESIÓN COMO ADMINISTRADOR
+                <Button href="/inicio_admin" className="boton-I">
+                  INICIAR SESIÓN COMO ADMINISTRADOR
                 </Button>
                 <br />
-                <Button  href="/registro" className="boton-I">
-                 REGISTRARSE
+                <Button href="/registro" className="boton-I">
+                  REGISTRARSE
                 </Button>
               </Form>
             </div>
